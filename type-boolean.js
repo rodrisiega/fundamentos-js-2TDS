0660c23e54ae179dc0107fce8d25a6b04227ea1c
Rodrigo Siega <rodrigo.siega@escola.pr.gov.br>
2025-09-08 16:42:51 -0300

    first commit

--- type-boolean.js
+++ type-boolean.js
@@ -20,3 +20,8 @@ console.log(texto1 === texto2); //case sensitive
 //git init - para criar um novo repositório vazio dentro dessa pasta
 //git status - mostra os arquivos (em vermelho) que ainda não foram enviados na nuvem
 //git remote add origin e o endereço do repositório criado no git
+//Na sequência 3 comandos que são os padrões do github
+//git add .  - para adicionar todos os arquivos no pack que será mandado para a nuvem
+// pode dar um git status para verificar
+//git commit -m "exemplos da aula 1"
+//git push origin master
